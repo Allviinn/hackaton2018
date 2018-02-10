0e2b00606d49591704cec52244da58deb0cb6844
Alvin <mujkicalvin1@gmail.com>
2018-02-10 12:40:59 +0100

    Nouveau thème bootstrap

--- SP.MVC/App_Start/BundleConfig.cs
+++ SP.MVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SP.MVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/newTheme.css",
                       "~/Content/site.css"));
         }
     }
