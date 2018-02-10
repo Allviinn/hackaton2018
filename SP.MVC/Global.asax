8990027e36950b8433cded972ceb93f8ea511c27
Victor MASSON <victor.masson@diiage.org>
2018-02-10 11:11:47 +0100

    Add project SuiviParcelle

---
+++ SP.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SP.MVC.MvcApplication" Language="C#" %>
---
+++ SP.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SP.MVC.Startup))]
+namespace SP.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
